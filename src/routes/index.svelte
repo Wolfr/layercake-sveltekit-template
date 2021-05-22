be1b599336571882459e49dfb82d05632e570153
Wolfr <johan.ronsse@gmail.com>
2021-05-22 12:59:06 +0200

    More examples
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,35 +1,25 @@
-<script>
-  import { LayerCake, Svg, Html, Canvas } from 'layercake';
-
-  import AxisX from '$lib/components/AxisX.svelte';
-  import AxisY from '$lib/components/AxisY.svelte';
-  import Line from '$lib/components/Line.svelte';
-
-  const data = [{ x: 0, y: 1 }, { x: 1, y: 2 }];
-</script>
-
 <style>
-  /* Set dimensions on the parent.
-     The <LayerCake> component will expand to fill it.
-   */
-  .chart-container {
-    width: 100%;
-    height: 500px;
+
+  .container {
+    max-width: 500px;
+    padding: 20px;
+    margin: 0 auto;
     font-family: sans-serif;
+    line-height: 1.5;
   }
 </style>
 
-<div class="chart-container">
-  <LayerCake
-    x='x'
-    y='y'
-    {data}
-  >
-  <AxisX />
-    <Svg>
-      <AxisX/>
-      <AxisY/>
-      <Line color='#f0c'/>
-    </Svg>
-  </LayerCake>
+<div class="container">
+  <h2>Layercake + Svelte kit template</h2>
+
+  <p>View examples:</p>
+
+  <ul>
+    <li><a href="/bar">Bar example</a></li>
+    <li><a href="/line-area">Line + area example</a></li>
+  </ul>
+
+  <p>Visit <a href="https://layercake.graphics/">layercake.graphics</a> for more information.</p>
+
+  <p>This repo mostly for trying to get the examples to work with SvelteKit, pull requests welcome with more examples!</p>
 </div>
